ef457e73dbe4aad2c7ea2353f5685c9c29f18ed1
Samantha Ostrowski <isphinxs@hotmail.com>
2024-03-10 15:50:47 -0400

    Use hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 import Home from './components/home/home';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
         {/* <Route path="share" /> */}
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
